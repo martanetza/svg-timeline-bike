484fd865a7ffc033dfd042fa44a1eab87c045f27
martanetza <39929716+martanetza@users.noreply.github.com>
2019-05-09 16:37:18 +0200

    some more animation

--- script.js
+++ script.js
@@ -54,10 +54,36 @@ function animate_1() {
     },
     {
       // opacity: 0,
-      delay: 5,
+      delay: 7,
       opacity: 1,
       rotation: -360,
       scale: 1
+    }
+  );
+  TweenMax.fromTo(
+    ".svgplaceholder",
+    1,
+    {
+      scale: 0,
+      transformOrigin: "50% 50%"
+    },
+    {
+      // opacity: 0,
+      delay: 8,
+      opacity: 1,
+      // rotation: 360,
+      scale: 1
+    }
+  );
+  TweenMax.fromTo(
+    ".specialShadow",
+    1,
+    {
+      opacity: 0
+    },
+    {
+      delay: 9,
+      opacity: 0.4
     }
   );
   TweenMax.to(".cls-2", 3, {
@@ -67,17 +93,17 @@ function animate_1() {
   });
 
   TweenLite.from(".cls-3", 2, {
-    delay: 3,
+    delay: 8,
     y: "-=519",
     ease: Elastic.easeOut.config(0.5, 0.3)
   });
   TweenLite.from(".cls-4", 2, {
-    delay: 3,
+    delay: 8,
     y: "-=519",
     ease: Elastic.easeOut.config(0.5, 0.3)
   });
   TweenLite.from(".cls-14", 2, {
-    delay: 3,
+    delay: 8,
     y: "-=519",
     ease: Elastic.easeOut.config(0.5, 0.3)
   });
@@ -85,31 +111,85 @@ function animate_1() {
   TweenLite.to(".cls-4", { y: "-=0" });
   TweenLite.to(".cls-14", { y: "-=0" });
   let animateArray = document.querySelectorAll(".animCircle");
+  let animateArrayGroup = document.querySelectorAll(".animCircleGroup");
+  let animatePlaceholder = document.querySelectorAll(".svgplaceholder");
 
-  console.log(animateArray);
-  let i;
+  animatePlaceholder.forEach(myFunct);
+  animateArrayGroup.forEach(myFunct);
 
-  for (let i = 0; i < animateArray.length; i++) {
-    console.log(i);
-    // i++;
-    let element = animateArray[i];
-    TweenMax.to(element, 1, { delay: i, opacity: 1 });
-    console.log(element);
+  function myFunct(elm, i) {
+    elm.addEventListener("mouseover", () => {
+      console.log(i);
+      let elm = animateArray[i];
+      let elmGroup = animateArrayGroup[i];
 
-    animateArray[i].addEventListener("mouseover", () => {
+      console.log(animateArrayGroup[i].getBoundingClientRect().top);
+
+      let differ = animatePlaceholder[i].getBoundingClientRect().top;
+      animatePlaceholder[i].addEventListener("click", () => {
+        console.log("click");
+        TweenMax.to(elmGroup, 3, {
+          rotation: -360,
+          // repeat: -1 /* Aka infinite amount of repeats */,
+          stroke: "#6c8f80",
+          strokeWidth: "1px",
+          strokeDasharray: "6",
+          strokeDashoffset: "3",
+          ease: Linear.easeNone,
+          // transform: "translate(100,100)"
+          y: -differ + differ / 2
+          // y: -differ / 2
+        });
+      });
+
       console.log("hehe");
       TweenMax.fromTo(
-        element,
-        1,
+        elm,
+        3,
         {
           transformOrigin: "50% 50%",
-          rotation: 0
+          rotation: 360,
+          stroke: "#6c8f80",
+          strokeWidth: "1px",
+          strokeDasharray: "6",
+          strokeDashoffset: "3"
         },
         {
-          rotation: -360
+          rotation: -360,
+          // repeat: -1 /* Aka infinite amount of repeats */,
+          stroke: "#6c8f80",
+          strokeWidth: "1px",
+          strokeDasharray: "6",
+          strokeDashoffset: "3",
+          ease: Linear.easeNone
+          // transform: "translate(100,100)"
+          // y: -differ + 100
+          // y: -differ / 2
         }
       );
     });
+    elm.addEventListener("mouseout", () => {
+      console.log(i);
+      let elm = animateArray[i];
+
+      console.log("hehe");
+      TweenMax.to(
+        elm,
+        1,
+
+        {
+          rotation: -360,
+          stroke: "none"
+        }
+      );
+    });
+  }
+
+  console.log(animateArray);
+  let i;
+  for (let i = 0; i < animateArray.length; i++) {
+    let element = animateArray[i];
+    TweenMax.to(element, 1, { delay: i / 1.6, opacity: 1 });
   }
 
   // TweenMax.to('.box', 3, {opacity: 1, scale: 1, ease: Elastic.easeOut.config(1, 0.3)});
@@ -130,8 +210,8 @@ function repalceSVGwithHTML(htmlElement) {
   // console.log(svgPlaceholder);
   const svgId = htmlElement.dataset.svgplaceholder;
   const svgSelector = "#" + svgId;
-  console.log(svgId);
-  console.log(htmlElement);
+  // console.log(svgId);
+  // console.log(htmlElement);
   const svgElement = document.querySelector(svgSelector);
 
   fitRetangule(svgElement, htmlElement);
@@ -148,9 +228,37 @@ function fitRetangule(svgElement, htmlElement) {
   // htmlElement.style.width = svgElement.getAttribute("width") + "px";
   // htmlElement.style.height = svgElement.getAttribute("height") + "px";
 
-  htmlElement.style.left = rect.x - headerWidth + 0.14 * headerWidth + "px";
+  htmlElement.style.left = rect.x - headerWidth + headerWidth / 5 + "px";
   htmlElement.style.top = rect.y + "px";
 
   htmlElement.style.width = rect.width + "px";
   htmlElement.style.height = rect.height + "px";
+}
+
+function loadJSON() {
+  fetch("data.json")
+    .then(res => res.json())
+    .then(data => showJSONdata(data));
+}
+
+loadJSON();
+
+function showJSONdata(data) {
+  console.log(data);
+
+  template = document.querySelector("template");
+  data.forEach(element => {
+    let clone = template.cloneNode(true).content;
+    clone.querySelector("h1").textContent = element.time;
+    // clone.querySelector("img").src =
+    //   "svg_plus_json/" + element.image + ".jpg";
+    // clone.querySelector("[data-field='year']").textContent = element.year;
+    // clone.querySelector("[data-field='director']").textContent =
+    //   element.director;
+    console.log(element.id);
+
+    document
+      .querySelector("[data-svgplaceholder='date_" + element.id + "']")
+      .appendChild(clone);
+  });
 }
